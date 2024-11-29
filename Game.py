2f9d3284b933a4c68b7504ced5c73f5ced85b70f
HappyH0tP0t <langji@lilangji.com>
2024-11-29 10:03:44 -0500

    Color

--- Game.py
+++ Game.py
@@ -10,12 +10,21 @@ running = True
 
 #Variables
 
+#Color
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+
 # Loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill((255, 255, 255))
+
+    pygame.draw.circle(screen, red, (250, 250), 250)
+    
     pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
