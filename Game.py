ab1f1cd053b85fc276ad1299c1b5269563527106
voltcutter <93495088+voltcutter@users.noreply.github.com>
2024-11-29 10:05:20 -0500

    Add useful variables

--- Game.py
+++ Game.py
@@ -1,20 +1,39 @@
 import pygame
+import time
 
 #Set up
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((960, 540))
 clock = pygame.time.Clock()
 running = True
 
 ##Main
 
 #Variables
+lastFrameMouseX, lastFrameMouseY = pygame.mouse.get_pos()
+mouseX, mouseY = pygame.mouse.get_pos()
+lastFrameTime = time.time()
 
 # Loop
 while running:
+    # calculate deltatime
+    currentTime = time.time()
+    deltaTime = currentTime - lastFrameTime
+    lastFrameTime = currentTime
+    # calculate mouse deltas
+    mouseX, mouseY = pygame.mouse.get_pos()
+    mouseDeltaX, mouseDeltaY = mouseX - lastFrameMouseX, mouseY - lastFrameMouseY
+    lastFrameMouseX, lastFrameMouseY = mouseX, mouseY
+
+    # debbug
+    print(deltaTime)
+
+    # keep an eye out for quitting
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # update screen (do this last)
     screen.fill((255, 255, 255))
     pygame.display.flip()
 
